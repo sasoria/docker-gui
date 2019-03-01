cec529c5503a06deaca97130f13001a0f7498f37
sasoria <sergio_s86@hotmail.com>
2019-03-01 18:11:37 +0100

    Added buttons

--- app/gui/app_window.py
+++ app/gui/app_window.py
@@ -8,7 +8,7 @@ class Window(Gtk.ApplicationWindow):
     def __init__(self, app):
         Gtk.Window.__init__(self, title="dockger-gui", application=app)
         self.set_border_width(3)
-        self.set_default_size(200, 100)
+        self.set_default_size(400, 200)
 
         self.notebook = Gtk.Notebook()
         self.add(self.notebook)
@@ -17,18 +17,56 @@ class Window(Gtk.ApplicationWindow):
         self.page1 = Gtk.ListBox()
         self.page1.set_border_width(10)
         for container in app.containers:
-            self.page1.add(Gtk.Label(container.__str__()))
+            row = Gtk.ListBoxRow()
+            box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=200)
+
+            row.add(box)
+
+            label = Gtk.Label(container.__str__())
+            label.set_justify(Gtk.Justification.FILL)
+            label.set_line_wrap(True)
+
+            button = Gtk.Button.new_with_label("run")
+            button.connect("clicked", self.on_click_run, container)
+
+            box.pack_start(label, True, True, 0)
+            box.pack_start(button, True, True, 0)
+
+            self.page1.add(row)
 
         # Images
         self.page2 = Gtk.ListBox()
         self.page2.set_border_width(10)
         for image in app.images:
-            self.page2.add(Gtk.Label(image.__str__()))
+            row = Gtk.ListBoxRow()
+            box = Gtk.Box(orientation=Gtk.Orientation.HORIZONTAL, spacing=200)
+
+            row.add(box)
+
+            label = Gtk.Label(image.__str__())
+            label.set_justify(Gtk.Justification.FILL)
+            label.set_line_wrap(True)
+
+            button = Gtk.Button.new_with_label("run")
+            button.connect("clicked", self.on_click_run, image)
+
+            box.pack_start(label, True, True, 0)
+            box.pack_start(button, True, True, 0)
+
+            self.page2.add(row)
 
         # TODO : Add inspect command in a button. Should display ports (Networksettings.Ports).
 
         self.notebook.append_page(self.page1, Gtk.Label("Containers"))
         self.notebook.append_page(self.page2, Gtk.Label("Images"))
+
+    def on_click_inspect(self, button, container):
+        print(container.__str__)
+        print(container.id)
+
+    def on_click_run(self, button, image):
+        print(image.__str__())
+        print(image.image_id)
 
 
 class Application(Gtk.Application):

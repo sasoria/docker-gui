1c8d89cb7d96a8a42f26fe32996075eda4f36269
sasoria <sergio_s86@hotmail.com>
2019-03-06 15:05:36 +0100

    Added listbox

--- app/gui/inspect_window.py
+++ app/gui/inspect_window.py
@@ -2,26 +2,29 @@ import gi.repository.Gtk as Gtk
 
 
 class InspectWindow(Gtk.Window):
-    def __init__(self, container_data):
+    def __init__(self, data):
         Gtk.Window.__init__(self, title="inspect container")
         self.set_border_width(3)
         self.set_default_size(800, 400)
+        self.listbox = Gtk.ListBox()
 
-        self.add(self._create_label(container_data))
+        for label in self._create_labels(data):
+            self.listbox.add(label)
 
-    def _create_label(self, data):
-        label = Gtk.Label()
-        #label.set_markup(self.create_inspected_info())
-        self._print_container(data)
-        return label
+        self.add(self.listbox)
 
-    def _create_data_string(self):
-        data_string = ""
+    def _create_labels(self, data):
+        labels = []
+        self._print_container(data, labels)
 
-    def _print_container(self, data):
+        return labels
+
+    def _print_container(self, data, labels):
         for k, v in data.items():
             if isinstance(v, dict):
-                self._print_container(v)
+                labels.append(Gtk.Label(k))
+                print("{0}: ".format(k))
+                self._print_container(v, labels)
             else:
                 print("{0}:{1}".format(k, v))
-
+                labels.append(Gtk.Label("{0}:{1}".format(k, v)))
